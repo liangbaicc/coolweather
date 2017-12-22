d7b3af988d02ba960a640a98dc58205e181b0342
liangbai <804432846@qq.com>
2017-12-22 12:19:03 +0800

    b

---
+++ app/src/main/java/android/coolweather/com/coolweather/util/HttpUtil.java
@@ -0,0 +1,19 @@
+package android.coolweather.com.coolweather.util;
+
+/**
+ * Created by CY on 2017/12/22.
+ */
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
